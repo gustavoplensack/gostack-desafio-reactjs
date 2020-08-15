9aae6cc4d4e3be6b06ea00eae5e4a006a79f7529
Gustavo <gustavoplensack@gmail.com>
2020-08-15 15:12:52 -0300

    Connect dashboard to API

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,12 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  /**
+   * Returns a value formated as a BRL currency
+   * @param value number to be converted in Currency.
+   */
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+};
 
 export default formatValue;
